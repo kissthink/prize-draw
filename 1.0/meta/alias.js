cfed404b20a9ecaaf95bd047e53189a27069b421
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-30 08:03:10 +0800

    sync daily branch 2.0.1

--- 1.0/meta/alias.js
+++
@@ -1,3 +0,0 @@
-config({
-    'gallery/prize-draw/index': {alias: ['gallery/prize-draw/1.0/index']}
-});
--- doc/meta/alias.js
+++
@@ -1,3 +0,0 @@
-config({
-    'kg/prize-draw/index': {alias: ['kg/prize-draw/2.0.0/index']}
-});
